3d503569068830f1fe508bdb96fcca5dfe1862a4
Donghwi_Park <gptnc5100@gmail.com>
2024-10-05 21:14:06 +0200

    Add project files.

---
+++ Maui_Project_CodeQuotes/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui_Project_CodeQuotes
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
