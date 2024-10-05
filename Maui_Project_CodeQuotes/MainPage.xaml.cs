e0b2a1fd95fc979f96fd5dce141ef4114255e3c2
Donghwi_Park <gptnc5100@gmail.com>
2024-10-05 22:16:14 +0200

    generated a random quote.

--- Maui_Project_CodeQuotes/MainPage.xaml.cs
+++ Maui_Project_CodeQuotes/MainPage.xaml.cs
@@ -2,9 +2,26 @@
 {
     public partial class MainPage : ContentPage
     {
+        List<string> quotes = new List<string>();
         public MainPage()
         {
             InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await LoadMauiAsset();
+        }
+
+        async Task LoadMauiAsset()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("quotes.txt");
+            using var reader = new StreamReader(stream);
+
+            while (reader.Peek() != -1)
+            {
+                quotes.Add(reader.ReadLine());
+            }
         }
 
         private void btnGenerateQuote_Clicked(object sender, EventArgs e)
@@ -37,9 +54,7 @@
             gstop5.Color = randomColor5;
 
             int index = Random.Shared.Next(quotes.Count);
-
             quote.Text = quotes[index];
         }
     }
-
 }
